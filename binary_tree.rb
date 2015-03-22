5268ba28dca1f982ec23ffda5aebc8b994216c42
ShIxtan <aulbrich@iu.edu>
2015-03-22 16:40:35 -0700

    add dfs to binary tree

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,9 +1,21 @@
 class TreeNode
-  attr_accessor :parent, :left, :right
+  attr_accessor :parent, :left, :right, :value
 
   def initialize(value, parent = nil)
+    @value = value
     @left = nil
     @right = nil
     @parent = parent
+  end
+
+  def leaf?
+    return true unless @left || @right
+  end
+
+  def dfs(target)
+    return self if value == target
+    return nil if leaf?
+
+    return dfs(@left) || dfs(@right)
   end
 end
